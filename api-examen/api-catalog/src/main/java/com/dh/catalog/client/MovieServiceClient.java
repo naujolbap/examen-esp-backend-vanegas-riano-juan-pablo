6b44509f28c3affe4d1f5f64f2916cf15e62a001
Juan Pablo V. Riano <valholl95@gmail.com>
2023-06-01 16:09:01 -0500

    Add Feign (RestClient) and Load Balancer in Catalog API

--- api-examen/api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
+++ api-examen/api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
@@ -1,23 +1,26 @@
 package com.dh.catalog.client;
 
+import com.dh.catalog.config.LoadBalancerConfig;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name="api-movie",url = "http://localhost:8085")
+@FeignClient(name="movie-service")
+@LoadBalancerClient(value = "movie-service", configuration = LoadBalancerConfig.class)
 public interface MovieServiceClient {
 
 	@GetMapping("/api/v1/movies/{genre}")
 	List<MovieDto> getMovieByGenre(@PathVariable (value = "genre") String genre);
 
-
 	@Getter
 	@Setter
 	class MovieDto{
+
 		private Long id;
 
 		private String name;
